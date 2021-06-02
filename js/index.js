078dae21beceaaf1d1d7033029c82272cc1c2950
LeonChisum <leonchisum3306@gmail.com>
2021-06-02 12:30:57 -0400

    changed spacing in header

--- js/index.js
+++ js/index.js
@@ -4,11 +4,13 @@ let menuBtn = document.getElementsByClassName('menu-btn');
 let xBtn = document.getElementsByClassName('x-btn');
 
 let mobileMenu = document.getElementsByClassName('mobile-menu');
+let mobileMenu2 = document.getElementsByClassName('mobile-menu2');
 
 let activeMenu = document.getElementsByClassName('active');
 
 let clickedBtn = function () {
 	mobileMenu[0].classList.toggle('active');
+	mobileMenu2[0].classList.toggle('active');
 };
 
 menuBtn[0].addEventListener('click', clickedBtn);
@@ -16,3 +18,4 @@ menuBtn[0].addEventListener('click', clickedBtn);
 // mobileMenu[0].addEventListener('click', clickedBtn);
 
 xBtn[0].addEventListener('click', clickedBtn);
+xBtn[1].addEventListener('click', clickedBtn);
