d648b5dc820fd95d6b71c8aa0b3b440e38e34e4e
LeonChisum <leonchisum3306@gmail.com>
2021-06-02 10:16:15 -0400

    finished header menu animation

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
 // functionality for the mobile menu responsive view
 let menuBtn = document.getElementsByClassName('menu-btn');
 
+let xBtn = document.getElementsByClassName('x-btn');
+
 let mobileMenu = document.getElementsByClassName('mobile-menu');
 
 let activeMenu = document.getElementsByClassName('active');
@@ -8,6 +10,9 @@ let activeMenu = document.getElementsByClassName('active');
 let clickedBtn = function () {
 	mobileMenu[0].classList.toggle('active');
 };
+
 menuBtn[0].addEventListener('click', clickedBtn);
 
-mobileMenu[0].addEventListener('click', clickedBtn);
+// mobileMenu[0].addEventListener('click', clickedBtn);
+
+xBtn[0].addEventListener('click', clickedBtn);
